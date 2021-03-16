5c52bbb981bf1617f1d02058ecc9617baa78066f
benvinert <benvinerttt@gmail.com>
2021-03-16 15:47:10 +0200

    change ALLOWED_HOSTS

--- backend/vinerfia_Project/vinerfia_Project/settings.py
+++ backend/vinerfia_Project/vinerfia_Project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'dh5+sfb$ae*fp8m7bbw3pqx=1s70f)ma=uq#ebe_9zxt2&9&v^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1","ben-ecommerce.herokuapp.com/"]
 
 
 # Application definition
