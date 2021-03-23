7ba49b8336664ada7d4b8adaa2a47dd4425c3bb1
benvinert <benvinerttt@gmail.com>
2021-03-23 16:57:04 +0200

    Fixed All URls

--- frontend/src/Authentication/Layout.js
+++ frontend/src/Authentication/Layout.js
@@ -28,7 +28,7 @@ export default function Layout()
     useEffect(async() => {
         let token = localStorage.getItem("access")
         if(token){
-        const req = await fetch("/auth/jwt/verify/",
+        const req = await fetch("https://backend-ben-ecommerce.herokuapp.com/auth/jwt/verify/",
         {
           method: 'POST', // *GET, POST, PUT, DELETE, etc.
           mode: 'cors', // no-cors, *cors, same-origin
