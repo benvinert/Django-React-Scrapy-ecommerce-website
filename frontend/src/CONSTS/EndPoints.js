614f4f7f41e64ee9ecf783a6c52f4bf98460784e
benvinert <benvinerttt@gmail.com>
2021-08-25 23:02:20 +0300

    add more CONSTANTS

--- frontend/src/CONSTS/EndPoints.js
+++ frontend/src/CONSTS/EndPoints.js
@@ -3,6 +3,9 @@ export const EndPoints = {
   ADMIN_GET_ALL_USERS: "api/Admin/getAllUsers",
   GET_ITEM_BY_ID: "api/All/getItemById/product_code=",
   GET_ALL_ITEMS: "api/All",
+  GET_USER_DATA: "auth/users/me",
+  CREATE_JWT_TOKEN: "auth/jwt/create/",
+  ACTIVATE_JWT_TOKEN: "auth/users/activation/",
 };
 
 export const Authorization = {
