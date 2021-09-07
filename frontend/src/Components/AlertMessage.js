c8f4eda8dcba6c911fb06b1c419f3142740b4821
benvinert <benvinerttt@gmail.com>
2021-09-07 23:22:45 +0300

    serialize

--- frontend/src/Components/AlertMessage.js
+++ frontend/src/Components/AlertMessage.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react'
-import Snackbar from '@material-ui/core/Snackbar';
+import React, { useState } from "react";
+import Snackbar from "@material-ui/core/Snackbar";
 
+const AlertMessage = ({ showAlert, payload }) => {
+  const [state, setState] = useState({
+    open: true,
+    vertical: "top",
+    horizontal: "center",
+  });
 
-const AlertMessage = ({showAlert,payload}) => 
-{
-    
-    const [state, setState] = React.useState({
-        open: true,
-        vertical: 'top',
-        horizontal: 'center',
-      });
-    
-    const { vertical, horizontal, open } = state;
+  const { vertical, horizontal, open } = state;
 
-    const AlertShow = () => 
-    {
-        if(showAlert.flag)
-        {
-            return <Snackbar
-            anchorOrigin={{ vertical, horizontal }}
-            open={open}
-            message={showAlert.message}
-            key={vertical + horizontal}
-            />
-        }
-        return null;
-        
+  const AlertShow = () => {
+    if (showAlert.flag) {
+      return (
+        <Snackbar
+          anchorOrigin={{ vertical, horizontal }}
+          open={open}
+          message={showAlert.message}
+          key={vertical + horizontal}
+        />
+      );
     }
+    return null;
+  };
 
-
-
-    return <AlertShow/>;
-}
+  return <AlertShow />;
+};
 
 export default AlertMessage;
